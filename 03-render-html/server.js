7bb0fad79edd567c8ea23e3ae072f85cb8ddc503
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2026-01-19 14:23:04 +0200

    Add 03 render html

---
+++ 03-render-html/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+function onRequest(request, response)
+{
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error)
+        {
+            response.writeHead(404);
+            response.write('File not found!');
+        }
+        else
+        {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+        }
+
+        response.end();
+    });
+}
+
+http.createServer(onRequest).listen(8000);
